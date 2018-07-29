632c3fd1daaee6119523afacf8f05d1d96c12b79
Josh Pabalan <joshpabalan@gmail.com>
2018-07-30 00:15:22 +0800

    Update 1

--- assets/odometer.js
+++ assets/odometer.js
@@ -639,7 +639,7 @@ window.odometerOptions = {
   // format: '(,ddd).dd', // Change how digit groups are formatted, and how many digits are shown after the decimal point
   // duration: 3000, // Change how long the javascript expects the CSS animation to take
   // theme: 'car', // Specify the theme (if you have more than one theme css file on the page)
-//   animation: 'count' // Count is a simpler animation method which just increments the value,
+  animation: 'count' // Count is a simpler animation method which just increments the value,
                      // use it when you're looking for something more subtle.
                     };
 
